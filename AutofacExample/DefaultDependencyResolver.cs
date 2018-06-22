bafd46bc0cf97d5ebb90aacbce52584423b65c7c
Alex Lindgren <alexlindgren@outlook.com>
2018-06-22 00:14:28 -0400

    Autofac setup WIP

---
+++ AutofacExample/DefaultDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutofacExample
+{
+    public class DefaultDependencyResolver : IDependencyResolver
+    {
+        protected IServiceProvider serviceProvider;
+
+        public DefaultDependencyResolver(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.serviceProvider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.serviceProvider.GetServices(serviceType);
+        }
+    }
+}
---
+++ AutofacExample/Startup.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Mvc;
+
+[assembly: OwinStartupAttribute(typeof(AutofacExample.Startup))]
+namespace AutofacExample
+{
+    
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            var resolver = new DefaultDependencyResolver(services.BuildServiceProvider());
+            DependencyResolver.SetResolver(resolver);
+
+            // http://scottdorman.github.io/2016/03/17/integrating-asp.net-core-dependency-injection-in-mvc-4/
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // todo: create services and add here
+
+            services.AddMediatR(typeof(Startup));
+        }
+    }
+}
